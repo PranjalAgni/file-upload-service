4ab5febbd2c1b5bbf166b697826d230a5fd640ce
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-10-03 22:36:48 +0530

    :seedling: replace random number generation with UUID

--- public/js/script.js
+++ public/js/script.js
@@ -4,17 +4,13 @@ const fileSelected = document.querySelector('#fileReader');
 const fileUploadButton = document.querySelector('#fileUpload');
 const BASE_API_URL = window.location.origin;
 
-const generateRandomNumber = () => {
-  const limit = Math.floor(Math.random() * 1000);
-  const number = Math.floor(Math.random() * limit);
-  return number;
-};
-
 fileUploadButton.addEventListener('click', (event) => {
   event.preventDefault();
   const fileReader = new FileReader();
   const file = fileSelected.files[0];
-  const fileName = `${generateRandomNumber()}-${file.name}`;
+  const uuid = window.crypto.randomUUID().split('-')[0];
+  const fileName = `${uuid}-${file.name}`;
+
   fileReader.onload = async (fileLoadEvent) => {
     console.log('File to upload: ', fileName);
     const CHUNK_SIZE = 1000;
@@ -38,6 +34,7 @@ fileUploadButton.addEventListener('click', (event) => {
       });
     }
   };
+
   // start reading file
   fileReader.readAsArrayBuffer(file);
 });
